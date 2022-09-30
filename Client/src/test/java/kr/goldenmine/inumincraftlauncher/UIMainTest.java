728f4e6eb11fd5367ba33ef2c7ec5e75fd31c3c0
GoldenMine0502 <ehe123@naver.com>
2022-09-30 13:46:25 +0900

    Core opens their server itself, deleted original src

--- Client/src/test/java/kr/goldenmine/inumincraftlauncher/UIMainTest.java
+++ Client/src/test/java/kr/goldenmine/inumincraftlauncher/UIMainTest.java
@@ -1,21 +1,11 @@
 package kr.goldenmine.inumincraftlauncher;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import kr.goldenmine.inuminecraftlauncher.CoreMain;
 import kr.goldenmine.inuminecraftlauncher.ui.MainFrame;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
 
 public class UIMainTest {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-//        SpringApplication.run(INUMinecraftLauncherMain.class, args);
-
-        SpringApplicationBuilder builder = new SpringApplicationBuilder(CoreMain.class);
-        builder.headless(false);
-        ConfigurableApplicationContext context = builder.run(args);
-
+        CoreMain.main(args);
         new MainFrame().setVisible(true);
     }
 }
--- Core/src/main/java/kr/goldenmine/inuminecraftlauncher/CoreMain.java
+++ Core/src/main/java/kr/goldenmine/inuminecraftlauncher/CoreMain.java
@@ -1,10 +1,18 @@
 package kr.goldenmine.inuminecraftlauncher;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CoreMain {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+//        SpringApplication.run(INUMinecraftLauncherMain.class, args);
 
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(CoreMain.class);
+        builder.headless(false);
+        ConfigurableApplicationContext context = builder.run(args);
     }
 }
